5c57f8de9571a861a234e8b5326f2ddf3a30499b
Vikas sharma <vikas@sovtechnologies.com>
2022-12-07 16:00:25 +0530

    init

--- src/api-service/interceptor.js
+++ src/api-service/interceptor.js
@@ -1,18 +1,16 @@
-import axios from "axios";
-import { authToken} from '../redux/reducers/login'
+// import axios from "axios";
+// import { authToken } from '../redux/reducers/login'
 
-console.log(989,authToken)
 
-axios.interceptors.request.use((req) => {
-    if (localStorage.getItem('accessToken')) {
 
-        req.headers.Authorization = authToken ? `Bearer ${authToken}` : "";
-    } else {
-        
-    }
-    return req;
-},
-    (err) => {
-        return Promise.reject(err)
-    }
-);
+// axios.interceptors.request.use((req) => {
+
+
+//     req.headers.Authorization = authToken ? `Bearer ${'dfdfgdfgdfgdf'}` : "";
+
+//     return req;
+// },
+//     (err) => {
+//         return Promise.reject(err)
+//     }
+// );
