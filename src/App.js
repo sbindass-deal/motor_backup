33bee3464092c2642f1011bc5399c8ae6d33dad4
Yash Mathur <65745613+theyashmathur@users.noreply.github.com>
2022-12-07 22:34:14 +0530

    wallet connect added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
 import "./App.css";
+import '@rainbow-me/rainbowkit/styles.css';
 import "./Assets/css/bootstrap.min.css";
 import "./Assets/css/style.css";
 import "./Assets/css/responsive.css";
 import Layout from "./Components/Pages/Layout";
 import axios from "axios";
 import { useSelector } from "react-redux";
+
+import {
+  connectorsForWallets,
+  darkTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import {
+  chain,
+  configureChains,
+  createClient,
+  useNetwork,
+  WagmiConfig,
+} from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import {
+  injectedWallet,
+  rainbowWallet,
+  walletConnectWallet,
+  metaMaskWallet,
+  ledgerWallet,
+  argentWallet,
+  braveWallet,
+  coinbaseWallet,
+  imTokenWallet,
+  trustWallet,
+  omniWallet
+} from "@rainbow-me/rainbowkit/wallets";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const bsc = {
+  id: 56,
+  name: "BSC",
+  network: "BNB Smartchain",
+  nativeCurrency: {
+    decimals: 18,
+    name: "BNB",
+    symbol: "BNB",
+  },
+  rpcUrls: {
+    default: "https://bsc-dataseed2.defibit.io",
+  },
+  blockExplorers: {
+    default: { name: "BscScan", url: "https://bscscan.com" },
+  },
+  testnet: false,
+};
+
+const { chains, provider } = configureChains(
+  [bsc, chain.mainnet],
+  [
+    infuraProvider({ apiKey: "dab1364f1304408b9d44f36d0773cf0a" }),
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== bsc.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+  ]
+);
+
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      injectedWallet({ chains }),
+      rainbowWallet({ chains }),
+      walletConnectWallet({ chains }),
+      metaMaskWallet({ chains }),
+      ledgerWallet({ chains }),
+      argentWallet({ chains }),
+      braveWallet({ chains }),
+      coinbaseWallet({ chains }),
+      imTokenWallet({ chains }),
+      trustWallet({ chains }),
+      omniWallet({ chains })
+    ],
+  },
+]);
+
+const client = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function App() {
   const logingUser = useSelector((state) => state.login);
@@ -20,9 +105,12 @@ function App() {
   );
 
   return (
-    <>
+    <WagmiConfig client={client}>
+          <RainbowKitProvider theme={darkTheme()} chains={chains}>
       <Layout />
-    </>
+      </RainbowKitProvider>
+
+    </WagmiConfig>
   );
 }
 
