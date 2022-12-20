c3690affb5cad78bfc0f4c4a7250213c26d673b4
Suchitra Desale <suchitra.desale29@gmail.com>
2022-12-20 18:55:47 +0530

    --init car slider
    --init car slider

--- src/Components/Pages/Dashboard/UserVehicleDetails.js
+++ src/Components/Pages/Dashboard/UserVehicleDetails.js
@@ -1,10 +1,18 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import SliderProduct from "../shopingStore/SliderProduct";
+
+import img_01 from "../../../Assets/images/th.jpeg";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import EastIcon from "@mui/icons-material/East";
+import WestIcon from "@mui/icons-material/West";
 
 const UserVehicleDetails = () => {
+  const slide = useRef(null);
   const { id } = useParams();
   const [vehicle, setVehicle] = useState({});
   const fetchVehicleApi = async () => {
@@ -22,6 +30,68 @@ const UserVehicleDetails = () => {
   useEffect(() => {
     fetchVehicleApi();
   }, [id]);
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    // autoplay: true,
+    // speed: 10000,
+    // pauseOnHover: true,
+    // cssEase: "linear"
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
 
   return (
     <div>
@@ -165,9 +235,108 @@ const UserVehicleDetails = () => {
                   </div>
               </div>
             </div>
+            
             <div className="col-12">
-             <h5>PHOTO GALLERY</h5>
-              <SliderProduct />
+             <h3>PHOTO GALLERY</h3>
+             <div className="col-12">
+              <div
+                style={{
+                  position: "absolute",
+                  right: "4%",
+                  cursor: "pointer",
+                  top: "-8%",
+                }}
+              >
+                <span onClick={() => slide.current.slickPrev()}>
+                  <span>
+                    <WestIcon />
+                  </span>
+                  <span>Prev</span>
+                </span>
+                <span
+                  onClick={() => slide.current.slickNext()}
+                  style={{ marginLeft: 50 }}
+                >
+                  <span>Next</span>
+                  <span style={{ height: "100px" }}>
+                    <EastIcon />
+                  </span>
+                </span>
+              </div>
+
+              <div
+                className="fore_Slide arrowTop_Slide"
+                style={{ height: "350px" }}
+              >
+                <Slider ref={slide} {...settings}>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_01" />
+                      </div>
+                     
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_02" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_03" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_04" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_01" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_02" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_03" />
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <div>
+                    <div className="card_post">
+                      <div className="card_postImg">
+                        <img src={img_01} alt="car_04" />
+                      </div>
+                     
+                    </div>
+                  </div>
+                </Slider>
+              </div>
+            </div>
             </div>
            
           </div>
