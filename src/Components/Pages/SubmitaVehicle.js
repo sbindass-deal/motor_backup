e57d70acf6565a13175015e565c55fba70f06d99
Roshan <roshan@sovtechnologies.com>
2023-01-05 13:36:06 +0530

    toggle

--- src/Components/Pages/SubmitaVehicle.js
+++ src/Components/Pages/SubmitaVehicle.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectPlan } from "../../redux/reducers/submitvechilesReducer";
 
-function SubmitaVehicle() {
+const SubmitaVehicle = () => {
+  const [planChacked, setPlanChacked] = useState({
+    standard: false,
+    pro: false,
+    premiere: false,
+    exclusive: false,
+  });
   const [planone, setp1] = React.useState(0);
   const [plansecond, setp2] = React.useState(0);
   const [planthird, setp3] = React.useState(0);
@@ -17,7 +23,9 @@ function SubmitaVehicle() {
       setp3(response.data.data[2].price);
     });
   }, []);
-
+  const handleOnChange = (e) => {
+    setPlanChacked({ ...planChacked, [e.target.name]: e.target.checked });
+  };
   return (
     <div>
       <section className="pt_80">
@@ -30,11 +38,10 @@ function SubmitaVehicle() {
               <p>
                 Gas Guzzlrs the best place to auction your vehicle.
                 <br /> Choose one of our many Auction Services to showease your
-                vehicle the way you want.{" "}
-                <Link to="/faq" className="link">
+                vehicle the way you want.
+                {/* <Link to="/faq" className="link">
                   Click here
-                </Link>
-                .
+                </Link> */}
               </p>
             </div>
             <div className="col-lg-3 col-md-3 col-sm-12 mb-4">
@@ -44,17 +51,20 @@ function SubmitaVehicle() {
                   <div className="plan_Price">
                     {/* ${planone} <span className="plan_Time">Month</span> */}
                     <div className="dfk">
-                      $99
+                      ${planChacked.standard ? 349 : 159}
                       <div className="switch">
                         <span className="plan_Time">Single Listing</span>
                         <input
                           className="react-switch-checkbox"
-                          id={`react-switch-new`}
+                          id={`react-switch-standard`}
+                          checked={planChacked.standard}
+                          onChange={handleOnChange}
+                          name="standard"
                           type="checkbox"
                         />
                         <label
                           className="react-switch-label"
-                          htmlFor={`react-switch-new`}
+                          htmlFor={`react-switch-standard`}
                         >
                           <span className={`react-switch-button`} />
                         </label>
@@ -115,17 +125,20 @@ function SubmitaVehicle() {
                     {/* ${plansecond}
                     <span className="plan_Time">Month</span> */}
                     <div className="dfk">
-                      $99
+                      ${planChacked.pro ? 399 : 199}
                       <div className="switch">
                         <span className="plan_Time">Single Listing</span>
                         <input
                           className="react-switch-checkbox"
-                          id={`react-switch-new`}
+                          id={`react-switch-pro`}
                           type="checkbox"
+                          checked={planChacked.pro}
+                          onChange={handleOnChange}
+                          name="pro"
                         />
                         <label
                           className="react-switch-label"
-                          htmlFor={`react-switch-new`}
+                          htmlFor={`react-switch-pro`}
                         >
                           <span className={`react-switch-button`} />
                         </label>
@@ -186,17 +199,20 @@ function SubmitaVehicle() {
                     {/* ${plansecond}
                     <span className="plan_Time">Month</span> */}
                     <div className="dfk">
-                      $349
+                      ${planChacked.premiere ? 799 : 499}
                       <div className="switch">
                         <span className="plan_Time">Single Listing</span>
                         <input
                           className="react-switch-checkbox"
-                          id={`react-switch-new`}
+                          id={`react-switch-premiere`}
                           type="checkbox"
+                          checked={planChacked.premiere}
+                          onChange={handleOnChange}
+                          name="premiere"
                         />
                         <label
                           className="react-switch-label"
-                          htmlFor={`react-switch-new`}
+                          htmlFor={`react-switch-premiere`}
                         >
                           <span className={`react-switch-button`} />
                         </label>
@@ -256,17 +272,24 @@ function SubmitaVehicle() {
                   <div className="plan_Price">
                     {/* ${planthird} <span className="plan_Time">Year</span> */}
                     <div className="dfk">
-                      <h3>Contact us</h3>
+                      <h5>
+                        {planChacked.exclusive
+                          ? "68767898943"
+                          : "Example@gmail.com"}
+                      </h5>
                       <div className="switch">
                         <span className="plan_Time">Single Listing</span>
                         <input
                           className="react-switch-checkbox"
-                          id={`react-switch-new`}
+                          id={`react-switch-exclusive`}
                           type="checkbox"
+                          checked={planChacked.exclusive}
+                          onChange={handleOnChange}
+                          name="exclusive"
                         />
                         <label
                           className="react-switch-label"
-                          htmlFor={`react-switch-new`}
+                          htmlFor={`react-switch-exclusive`}
                         >
                           <span className={`react-switch-button`} />
                         </label>
@@ -323,6 +346,6 @@ function SubmitaVehicle() {
       </section>
     </div>
   );
-}
+};
 
 export default SubmitaVehicle;
