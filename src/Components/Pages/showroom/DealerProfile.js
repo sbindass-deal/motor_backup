adff2109d798d27199df506fe1bebcfda01acb38
Suchitra Desale <nill.nikhil007@gmail.com>
2023-03-14 21:20:47 +0530

    --init
    --init

--- src/Components/Pages/showroom/DealerProfile.js
+++ src/Components/Pages/showroom/DealerProfile.js
@@ -7,6 +7,8 @@ import DealerVehicleList from "./DealerVehicleList";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import Carousal from "./Carousal";
+import Dabout from "../../../Assets/images/Dabout.jpg"
+import caer from "../../../Assets/images/caer.gif"
 
 const DealerProfile = () => {
   const { id } = useParams();
@@ -130,8 +132,52 @@ const DealerProfile = () => {
           </div>
         </div>
       </section>
+      <section className="pt_80 mobileSpec" id="">
+        <div className="container">
+          <div className="row ">
+              <div className="col-lg-6 col-md-6 col-sm-12" style={{padding: "0"}}>
+                <img src={Dabout} alt="aboutImg"/>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12 bgOrange">
+                 
+                    <h3>About Us</h3>
+                    <p>
+                    For nearly 20 years, residents throughout the local area have turned to Car Repair for all of their automotive repair needs. With free estimates and 
+fast turnaround, we are known for our personal service and expertise in all forms of specialized engine repair. We use the latest and most modern 
+diagnostic technology to have you back in your car.
+
+                    </p>
+                    <button className="btn">View More</button>
+                  
+              </div>
+          </div>
+        </div>
+      </section>
       <DealerAuction dealerName={dealerData.name} userId={id} />
       <DealerVehicleList dealerName={dealerData.name} userId={id} />
+      <section className="pt_80 mobileSpec contactD" id="">
+        <div className="container">
+          <div className="row ">
+            <div className="col-6">
+              <img src={caer}/>
+            </div>
+            <div className="col-6 ">
+              <div className="contactInfo">
+              <h2>CONTACTS US</h2>
+              <ul>
+                <li><i class="fa-solid fa-location-dot"></i> ADDRESS:</li>
+                <li>4578 MARMORA ROAD, GLASGOW D04 89GR</li>
+                </ul>
+                <ul>
+                <li><i class="fa-solid fa-phone"></i> NUMBER:</li>
+                <li>800-2345-6789</li>
+              </ul>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
