266ac04275feb664ac56c1e783b5939ddebe6108
Roshan <roshan@sovtechnologies.com>
2023-04-05 19:49:38 +0530

    init

--- src/Components/Pages/garages/GaragesUserDetails.js
+++ src/Components/Pages/garages/GaragesUserDetails.js
@@ -14,6 +14,7 @@ import Videos from "./Videos";
 import { noImage, strToHtml } from "../../UI/globaleVar";
 import parse from "html-react-parser";
 import LatestGuzzlrsAuction from "../auction/auctionDetails/LatestGuzzlrsAuction";
+import Auction from "./Auction";
 
 const GaragesUserDetails = () => {
   const [garagesData, setGaragesData] = useState({});
@@ -371,104 +372,7 @@ const GaragesUserDetails = () => {
                 aria-labelledby="profile-tab"
                 tabindex="0"
               >
-                <div className="row">
-                  <div className="col-md-12 ">
-                    <div className="PostInfo">
-                      <div className="userImG">
-                        <Space direction="vertical" size={16}>
-                          <Space wrap size={16}>
-                            <Avatar
-                              size={64}
-                              icon={<img src={men_face} alt="logo" />}
-                            />
-                          </Space>
-                        </Space>
-                      </div>
-                      <div className="DecIbp">
-                        <h5>User</h5>
-                        <p>
-                          This is a wider card with supporting text below as a
-                          natural lead-in to additional content. This content is
-                          a little bit longer.
-                        </p>
-                        <div class="card">
-                          <img
-                            src="https://tse1.mm.bing.net/th?id=OIP.cedHozvsh9JzkHQgRVg8XQHaE8&pid=Api&P=0"
-                            class="card-img-bottom"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="py-3">
-                          <span className="socialCount">
-                            <i className={`fa-solid fa-thumbs-up `}></i> 22
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-comments"></i> 2
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-share-from-square"></i> 12
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-heart"></i> 8,427
-                          </span>
-
-                          <span className="socialCount">
-                            <i class="fa-solid fa-eye"></i> 99k
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-12 ">
-                    <div className="PostInfo">
-                      <div className="userImG">
-                        <Space direction="vertical" size={16}>
-                          <Space wrap size={16}>
-                            <Avatar
-                              size={64}
-                              icon={<img src={men_face} alt="logo" />}
-                            />
-                          </Space>
-                        </Space>
-                      </div>
-                      <div className="DecIbp">
-                        <h5>User</h5>
-                        <p>
-                          This is a wider card with supporting text below as a
-                          natural lead-in to additional content. This content is
-                          a little bit longer.
-                        </p>
-                        <div class="card">
-                          <img
-                            src="https://tse1.mm.bing.net/th?id=OIP.cedHozvsh9JzkHQgRVg8XQHaE8&pid=Api&P=0"
-                            class="card-img-bottom"
-                            alt="..."
-                          />
-                        </div>
-                        <div className="py-3">
-                          <span className="socialCount">
-                            <i className={`fa-solid fa-thumbs-up `}></i> 22
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-comments"></i> 2
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-share-from-square"></i> 12
-                          </span>
-                          <span className="socialCount">
-                            <i class="fa-solid fa-heart"></i> 8,427
-                          </span>
-
-                          <span className="socialCount">
-                            <i class="fa-solid fa-eye"></i> 99k
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Auction />
               </div>
               <div
                 class="tab-pane fade"
