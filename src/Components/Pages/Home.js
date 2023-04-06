ae26c25498cc59314bb84f6ba358e7839216be6d
Vikas sharma <vikas@sovtechnologies.com>
2023-04-07 00:23:38 +0530

    init

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -300,7 +300,7 @@ function Home() {
         <div className="container">
           <div className="row">
             <div className="col-12 col-lg-4 offset-md-1 text-center text-md-left">
-              <h1 className="text-back">Change to Find your Special Vehicle</h1>
+              <h1 className="text-back">Find your Special Vehicle</h1>
               <Link to="/showroom" className="orange_btn mt-2">
                 View Featured
               </Link>
