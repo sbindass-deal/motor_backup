668983cacae47bb504d8f92466bfffd0f773b4ac
Roshan <roshan@sovtechnologies.com>
2023-04-20 00:46:31 +0530

    init

--- src/Components/Pages/auction/Analytics.js
+++ src/Components/Pages/auction/Analytics.js
@@ -41,13 +41,13 @@ export const data = {
   datasets: [
     {
       label: "Dataset 1",
-      data: labels.map(() => ({ min: -1000, max: 1000 })),
+      data: { min: -1000, max: 2000 },
       borderColor: "rgb(255, 99, 132)",
       backgroundColor: "rgba(255, 99, 132, 0.5)",
     },
     {
       label: "Dataset 2",
-      data: labels.map(() => ({ min: -1000, max: 1000 })),
+      data: { min: -800, max: 2000 },
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
