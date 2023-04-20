3e629959e6de230d03116ae8c35caaf7c87e8daf
Nikhil Patil <nill.nikhil007@gmail.com>
2023-04-20 08:27:48 +0530

    --init
    --init

--- src/Components/Pages/auction/Analytics.js
+++ src/Components/Pages/auction/Analytics.js
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+import Mitsubishi from "../../../Assets/images/Mitsubishi.webp"
 function Analytics() {
   return (
-    <div>Analytics</div>
-  )
+    <>
+    
+      <section className="ptb_80 pt_sm_50">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center pb_30">
+              <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                <Row>
+                  <Col sm={3}>
+                    <Nav variant="pills" className="flex-column">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">
+                            <div className="carImg">
+                                <img src={Mitsubishi}/>
+                                <p>Mitsubishi 3000GT</p>
+                            </div>
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </Col>
+                  <Col sm={9}>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="first">
+                        <p>rrrrrrrrr</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>pppppppppp</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Col>
+                </Row>
+              </Tab.Container>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Analytics
+export default Analytics;
