113f54a2e0dd6af22fa7ba3d222f1cdec3fe2a4a
Roshan <roshan@sovtechnologies.com>
2023-04-21 19:07:27 +0530

    get allert page

--- src/Components/Pages/GetAlerts.js
+++ src/Components/Pages/GetAlerts.js
@@ -4,6 +4,7 @@ import nr from "../../Assets/images/nr.png";
 import premium from "../../Assets/images/premium.png";
 import auction_list from "../../Assets/images/auction_list.jpg";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const GetAlerts = () => {
   const handleSubmitApi = () => {
@@ -24,13 +25,19 @@ const GetAlerts = () => {
               className="text title_combo title_Center"
               style={{ color: "#fff" }}
             >
-              Gas Guzzlrs Daily Mail
+              {/* Gas Guzzlrs Daily Mail */}
+              Daily Mail of the Gas Guzzlrs
             </h2>
-            <p className="title_combo title_Center" style={{ color: "#fff" }}>
-              All the most relevant new content on Gas Guzzlrs at 5am Pacific
-              each day, including:
+            <p
+              className="title_combo title_Center py-2"
+              style={{ color: "#fff" }}
+            >
+              {/* All the most relevant new content on Gas Guzzlrs at 5am Pacific
+              each day, including: */}
+              Every day at 5am Pacific, we curate and deliver the latest and
+              most pertinent Gas Guzzlrs content, ensuring you stay up-to-date
+              with the most relevant news.
             </p>
-
             <div className="row mt-4">
               <div className="col-md-8 offset-lg-2">
                 <div className="daily_mail_box">
@@ -64,10 +71,13 @@ const GetAlerts = () => {
                   <img src={results_digest} />
                 </div>
                 <div className="card_icBody">
-                  <h4>Results Digest</h4>
+                  {/* <h4>Results Digest</h4> */}
+                  <h4>Result Insights</h4>
                   <p>
-                    Yesterday’s completed auction results are sent to you at 5am
-                    Pacific each day.
+                    {/* Yesterday’s completed auction results are sent to you at 5am
+                    Pacific each day. */}
+                    You receive the results of the auction held the day before
+                    every morning at 5 a.m. Pacific time.
                   </p>
                   <button type="submit" className="results_subscribe btn">
                     <i className="fa-regular fa-envelope"></i>{" "}
@@ -82,10 +92,13 @@ const GetAlerts = () => {
                   <img src={nr} />
                 </div>
                 <div className="card_icBody">
-                  <h4>No Reserve Digest</h4>
+                  {/* <h4>No Reserve Digest</h4> */}
+                  <h4>No Reserve Insights</h4>
                   <p>
-                    No Reserve listings that went live in the past 24 hours are
-                    sent to you at 5pm Pacific each day.
+                    {/* No Reserve listings that went live in the past 24 hours are
+                    sent to you at 5pm Pacific each day. */}
+                    A daily summary of No Reserve listings that have gone live
+                    over the past 24 hours is emailed to you at 5pm Pacific.
                   </p>
                   <button type="submit" className="results_subscribe btn">
                     <i className="fa-regular fa-envelope"></i>{" "}
@@ -100,10 +113,13 @@ const GetAlerts = () => {
                   <img src={premium} />
                 </div>
                 <div className="card_icBody">
-                  <h4>Gas Guzzlrs Premium Listings</h4>
+                  {/* <h4>Gas Guzzlrs Premium Listings</h4> */}
+                  <h4>Premium Listings on Gas Guzzlrs</h4>
                   <p>
-                    Each Premium listing is sent to you as soon as the auction
-                    goes live.
+                    {/* Each Premium listing is sent to you as soon as the auction
+                    goes live. */}
+                    You will receive every Premium listing as soon as the
+                    auction goes live.
                   </p>
                   <button type="submit" className="results_subscribe btn">
                     <i className="fa-regular fa-envelope"></i>{" "}
@@ -123,31 +139,46 @@ const GetAlerts = () => {
               <img src={auction_list} />
             </div>
             <div className="col-12 col-lg-6 col-md-6 align-items-center pt_80 pb_80">
-              <h3>Watch an auction to follow the action start to finish.</h3>
+              {/* <h3>Watch an auction to follow the action start to finish.</h3> */}
+              <h3>
+                If you wish to follow an auction from beginning to end, you
+                should watch it
+              </h3>
               <p>
-                Click a Watch star Star icon to watch that listing. When you
-                watch a listing:
+                {/* Click a Watch star Star icon to watch that listing. When you
+                watch a listing: */}
+                Click the Watch Bell icon to watch that listing. Watching a
+                listing involves:
               </p>
               <ul className="dotList mb-3">
-                <li>It appears on My Watchlist.</li>
+                {/* <li>It appears on My Watchlist.</li> */}
+                <li>It's listed in My Watchlist.</li>
                 <li>
-                  New photos or videos added to the listing are emailed to you.
+                  {/* New photos or videos added to the listing are emailed to you. */}
+                  You receive emails when new images or videos are posted to the
+                  listing.
                 </li>
                 <li>
-                  A one-hour email reminder or 30 minute text reminder can be
-                  sent to you before the auction closes.
+                  {/* A one-hour email reminder or 30 minute text reminder can be
+                  sent to you before the auction closes. */}
+                  You can receive a 30-minute text or 1-hour email reminder
+                  before the auction closes.
                 </li>
                 <li>
-                  A summary of the auction result is emailed to you when it
-                  closes.
+                  {/* A summary of the auction result is emailed to you when it
+                  closes. */}
+                  When the auction concludes, you receive an email with a
+                  summary of the results.
                 </li>
                 <li>
-                  Well email you if the vehicle is listed again on Gas Guzzlrs.
+                  {/* Well email you if the vehicle is listed again on Gas Guzzlrs. */}
+                  If the vehicle is listed on Gas Guzzlrs once more, we'll email
+                  you.
                 </li>
               </ul>
-              <a className="btn" href="#">
+              <Link to="/settings" className="btn">
                 <i className="fa-solid fa-list mr-2"></i>Go to My Watchlist
-              </a>
+              </Link>
             </div>
           </div>
         </div>
--- src/Components/Pages/MyAccount/Settings.js
+++ src/Components/Pages/MyAccount/Settings.js
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from "react";
 import MyAccountLeftNav from "./MyAccountLeftNav";
 import axios from "axios";
 import { noImage } from "../../UI/globaleVar";
-import ClearIcon from '@mui/icons-material/Clear';
+import ClearIcon from "@mui/icons-material/Clear";
 import SmallSpinner from "../../UI/SmallSpinner";
 import { toast } from "react-toastify";
 function Settings() {
-  const [allData, setAllData] = useState([])
-  const [loading, setloading] = useState(true)
+  const [allData, setAllData] = useState([]);
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     const getSubscription = async () => {
       try {
         const res = await axios.post(
-          `${process.env.REACT_APP_URL}getWatchListByUserId`, {}
+          `${process.env.REACT_APP_URL}getWatchListByUserId`,
+          {}
         );
-        console.log(888190, res)
-        setAllData(res.data.data)
-        setloading(false)
+        setAllData(res.data.data);
+        setloading(false);
       } catch (err) {
         console.log(err);
       }
@@ -37,26 +37,20 @@ function Settings() {
       theme: "light",
     });
 
-
   const handleDelete = (id) => {
-    alert("Delete" + " " + id)
+    alert("Delete" + " " + id);
 
-    axios.post(`${process.env.REACT_APP_URL}removeWatchList/${id}`)
+    axios
+      .post(`${process.env.REACT_APP_URL}removeWatchList/${id}`)
       .then(function (response) {
         if (response.data.status === 200 && response)
-          console.log(10010, response);
-        notify(response.data.message);
+          notify(response.data.message);
         window.location.reload(false);
-
       })
       .catch(function (error) {
         console.log(10010, error);
       });
-
-  }
-
-
-
+  };
 
   return (
     <>
@@ -76,10 +70,10 @@ function Settings() {
               <ul className="rowList_ mb-4">
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway News and Updates</h6>
+                    <h6>Gas Guzzlrs Giveaway News and Updates</h6>
                     <p>
                       We'll email you about site updates, pro tips and all
-                      things Gass Guzzlrs Giveaway. We promise not to spam you.
+                      things Gas Guzzlrs Giveaway. We promise not to spam you.
                     </p>
                   </div>
                   <div className="ml-auto">
@@ -101,9 +95,9 @@ function Settings() {
                 </li>
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway Event Updates</h6>
+                    <h6>Gas Guzzlrs Giveaway Event Updates</h6>
                     <p>
-                      We'll email you about upcoming Gass Guzzlrs Giveaway
+                      We'll email you about upcoming Gas Guzzlrs Giveaway
                       events.
                     </p>
                   </div>
@@ -126,7 +120,7 @@ function Settings() {
                 </li>
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway Daily Mail</h6>
+                    <h6>Gas Guzzlrs Giveaway Daily Mail</h6>
                     <p>
                       We'll email you at 5am PST every day with the latest
                       listings, posts and calendar events.
@@ -150,7 +144,7 @@ function Settings() {
                 </li>
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway Results Digest</h6>
+                    <h6>Gas Guzzlrs Giveaway Results Digest</h6>
                     <p>
                       We'll email you at 5am PT every day with the previous
                       day's auction results.
@@ -174,10 +168,10 @@ function Settings() {
                 </li>
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway Market Updates</h6>
+                    <h6>Gas Guzzlrs Giveaway Market Updates</h6>
                     <p>
                       We'll occasionally email you data and reports featuring
-                      interesting Gass Guzzlrs Giveaway auction results.
+                      interesting Gas Guzzlrs Giveaway auction results.
                     </p>
                   </div>
                   <div className="ml-auto">
@@ -207,14 +201,28 @@ function Settings() {
                 options below.
               </p>
               <p>
-                {loading ? <SmallSpinner /> :
+                {loading ? (
+                  <SmallSpinner />
+                ) : (
                   allData?.map((curVal, i) => {
-                    return <p className="watchlistStyle">
-                      <p > {`${curVal.make} ${curVal.model} ${curVal.year}`}</p>
-                      <p><button className="ml-3 watchlistStyleBtn" onClick={() => handleDelete(curVal.vehicleId)}><ClearIcon /></button></p>
-                    </p>
+                    return (
+                      <p className="watchlistStyle">
+                        <p>
+                          {" "}
+                          {`${curVal.make} ${curVal.model} ${curVal.year}`}
+                        </p>
+                        <p>
+                          <button
+                            className="ml-3 watchlistStyleBtn"
+                            onClick={() => handleDelete(curVal.vehicleId)}
+                          >
+                            <ClearIcon />
+                          </button>
+                        </p>
+                      </p>
+                    );
                   })
-                }
+                )}
               </p>
               <ul className="rowList_ mb-4">
                 <li>
@@ -292,9 +300,9 @@ function Settings() {
                 </li>
                 <li>
                   <div className="pr-3">
-                    <h6>Gass Guzzlrs Giveaway History Updates</h6>
+                    <h6>Gas Guzzlrs Giveaway History Updates</h6>
                     <p>
-                      We'll email you when the vehicle is listed again on Gass
+                      We'll email you when the vehicle is listed again on Gas
                       Guzzlrs Giveaway.
                     </p>
                   </div>
@@ -319,7 +327,7 @@ function Settings() {
                     <h6>Text message notifications</h6>
                     <p>
                       We'll occasionally email you data and reports featuring
-                      interesting Gass Guzzlrs Giveaway auction results.
+                      interesting Gas Guzzlrs Giveaway auction results.
                     </p>
                   </div>
                   <div className="ml-auto">
@@ -361,7 +369,7 @@ function Settings() {
 
               <h5>Partner Page Subscriptions</h5>
               <p>
-                We'll email you about Gass Guzzlrs Giveaway Partners you're
+                We'll email you about Gas Guzzlrs Giveaway Partners you're
                 following. Your subscribed Partner pages are shown below.
               </p>
 
