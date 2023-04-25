ad974a1b0b8d26d4f50bc429d7dcdff851020c1f
Nikhil Patil <nill.nikhil007@gmail.com>
2023-04-25 14:38:20 +0530

    --init
    --init

--- src/Components/Pages/MyAccount/AccountInfo.js
+++ src/Components/Pages/MyAccount/AccountInfo.js
@@ -14,8 +14,6 @@ import parse from "html-react-parser";
 import { Image } from "antd";
 import userProfile from "../../../Assets/images/userProfile.jpeg"
 
-// import { Image } from "antd";
-import { Avatar, Image, Space } from "antd";
 function AccountInfo() {
   const userId = useSelector((state) => state);
   const [userInfo, setUserinfo] = useState({});
@@ -30,7 +28,6 @@ function AccountInfo() {
       const res = await axios.get(`${process.env.REACT_APP_URL}user`);
       if (res.data.data) {
         setUserinfo(res.data.data);
-        console.log(userInfo)
         setIsPrivateOrPublic(res.data.data.published == 0 ? false : true);
       } else {
         setUserinfo(userInfo);
@@ -124,44 +121,14 @@ function AccountInfo() {
                       ></label>
                     </div>
                   )} */}
-                  {/* <Link to="/editmyaccount" className="gry_btn px-3">
+                  <Link to="/editmyaccount" className="gry_btn px-3">
                     Edit
-                  </Link> */}
+                  </Link>
                 </div>
               </div>
               <hr />
               <div className="row">
               <div className="col-6">
-              <div className="UserImZ mt-4">
-                      <Space direction="vertical" size={16}>
-                        <Space wrap size={16}>
-                          <Avatar
-                            size={100}
-                            icon={
-                              <img
-                                className="slidImg"
-                                loading="lazy"
-                                src={
-                                  userInfo?.logo &&
-                                  `${process.env.REACT_APP_URL}/${userInfo?.logo[0]?.logo}`
-                                }
-                                onError={({ currentTarget }) => {
-                                  currentTarget.onError = null;
-                                  currentTarget.src = noImage;
-                                }}
-                                alt="Logo"
-                              />
-                            }
-                          />
-                        </Space>
-                        <h2 className="mt-2 mb-3">
-                      {userInfo.name}
-                      <Link to="/editmyaccount">
-                        <i class="fa-solid fa-pen-to-square"></i>
-                      </Link>
-                    </h2>
-                      </Space>
-                    </div>
               <ul className="labelList_">
                 <li className="avtar">
                   <span>
